74511ee70f0148c9f23663b1e19018ada26ba895
zhu2315 <67095520+zhu2315@users.noreply.github.com>
2021-01-13 17:53:22 +0800

    Create  创建一个带有简单部件的桌面应用.py

---
+++ 3，创建一个带有简单部件的桌面应用/创建一个带有简单部件的桌面应用.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+root = tk.Tk()
+label = tk.Label(root, text="My name is label.")
+button = tk.Button(root, text="My name is button.")
+label.pack()
+button.pack()
+root.mainloop()
